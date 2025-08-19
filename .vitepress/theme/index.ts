0ec1a02ee7544fd69ee108f798678cf37032cda0
Adi Roiban <adiroiban@gmail.com>
2025-08-19 14:01:54 +0100

    Add more icons.

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -6,6 +6,41 @@ import {config, library} from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 import {faMastodon, faSquareGithub} from '@fortawesome/free-brands-svg-icons'
 import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
+import {
+    faAdjust,
+    faAngleUp,
+    faBars,
+    faBook,
+    faCheck,
+    faCheckDouble,
+    faChevronRight,
+    faDisplay,
+    faEnvelope,
+    faExclamationCircle,
+    faEye,
+    faEyeSlash,
+    faFile,
+    faFolder,
+    faHandshake,
+    faHeadset,
+    faHome,
+    faLifeRing,
+    faMagnifyingGlass,
+    faMedal,
+    faMinus,
+    faMoon,
+    faPaperPlane,
+    faPeopleArrows,
+    faPeopleGroup,
+    faPlus,
+    faQuoteLeft,
+    faQuoteRight,
+    faScaleBalanced,
+    faSun,
+    faUser,
+    faWarning,
+    faXmark,
+} from '@fortawesome/free-solid-svg-icons'
 
 import Layout from './Layout.vue'
 import type { Theme } from 'vitepress'
@@ -14,7 +49,41 @@ import './style.css'
 // Config font awesome
 config.autoAddCss = false
 library.add(
+    faAdjust,
+    faAngleUp,
+    faBars,
+    faBook,
+    faCheck,
+    faCheckDouble,
+    faChevronRight,
+    faDisplay,
+    faEnvelope,
+    faExclamationCircle,
+    faEye,
+    faEyeSlash,
+    faFile,
+    faFolder,
+    faHome,
+    faHandshake,
+    faHeadset,
+    faLifeRing,
+    faMagnifyingGlass,
+    faMastodon,
+    faMedal,
+    faMinus,
+    faMoon,
+    faPaperPlane,
+    faPeopleArrows,
+    faPeopleGroup,
+    faPlus,
+    faQuoteLeft,
+    faQuoteRight,
+    faScaleBalanced,
     faSquareGithub,
+    faSun,
+    faUser,
+    faWarning,
+    faXmark
 )
 
 export default {
